a445ffa29b50cba0f5550851be79477b7531777c
Dion Garman <diongarman@gmail.com>
2020-04-08 18:03:00 +0100

    bad implementation of type guards

--- build/index.js
+++ build/index.js
@@ -4,10 +4,35 @@ var Sorter = /** @class */ (function () {
         this.collection = collection;
         this.collection = collection;
     }
-    Sorter.prototype.sort = function () { };
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - (i - 1); j++) {
+                //block only works if collection is number[]
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+                if (typeof this.collection === 'string') {
+                    console.log(j, this.collection);
+                    var left = this.collection[j].toLowerCase().charCodeAt(0);
+                    var right = this.collection[j + 1].toLowerCase().charCodeAt(0);
+                    if (left > right) {
+                        var temp = this.collection[j];
+                        this.collection.split('')[j] = this.collection[j + 1];
+                        this.collection.split('')[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    };
     return Sorter;
 }());
-var sorter = new Sorter([-1, 100, 18, 76, 0, 9]);
+//const sorter = new Sorter([-1, 100, 18, 76, 0, 9]);
+var sorter = new Sorter('Xaaaa');
 sorter.sort();
 //should be sorted
 console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
 class Sorter {
-  constructor(public collection: number[]) {
+  constructor(public collection: number[] | string) {
     this.collection = collection;
   }
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - (i - 1); j++) {
+        //block only works if collection is number[]
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = temp;
+          }
+        }
+
+        if (typeof this.collection === 'string') {
+          console.log(j, this.collection);
+          const left = this.collection[j].toLowerCase().charCodeAt(0);
+          const right = this.collection[j + 1].toLowerCase().charCodeAt(0);
+          if (left > right) {
+            const temp = this.collection[j];
+            this.collection.split('')[j] = this.collection[j + 1];
+            this.collection.split('')[j + 1] = temp;
+          }
+        }
+      }
+    }
+  }
 }
 
-const sorter = new Sorter([-1, 100, 18, 76, 0, 9]);
+//const sorter = new Sorter([-1, 100, 18, 76, 0, 9]);
+const sorter = new Sorter('Xaaaa');
 sorter.sort();
 //should be sorted
 console.log(sorter.collection);
