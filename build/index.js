20bd90c8e39aa1fd7aa849d6ad10a411cb800fcf
Dion Garman <diongarman@gmail.com>
2020-04-08 15:03:32 +0100

    sort class set up

--- build/index.js
+++ build/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Blicky got tha stiffy ugh');
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () { };
+    return Sorter;
+}());
+var sorter = new Sorter([-1, 100, 18, 76, 0, 9]);
+sorter.sort();
+//should be sorted
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Blicky got tha stiffy ugh');
+class Sorter {
+  constructor(public collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([-1, 100, 18, 76, 0, 9]);
+sorter.sort();
+//should be sorted
+console.log(sorter.collection);
