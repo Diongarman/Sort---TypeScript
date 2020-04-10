b1d44a39394a16edad4315fb40be515fe31d6ce2
Dion Garman <diongarman@gmail.com>
2020-04-10 19:42:17 +0100

    ideal client code (still to implement functionality)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { NumbersCollection } from './numbersCollection';
 import { CharactersCollection } from './charactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([-1000, 101, 18, 76, 0, 9]);
+const numbersCollection = new NumbersCollection([-1000, 101, 18, 76, 0, 9]);
+numbersCollection.sort();
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
@@ -13,13 +14,13 @@ import { LinkedList } from './LinkedList';
 // sorter.sort();
 // console.log(charactersCollection.data);
 
-const linkedListCollection = new LinkedList();
-linkedListCollection.add(1);
-linkedListCollection.add(100);
-linkedListCollection.add(-1);
-linkedListCollection.add(-115);
-linkedListCollection.add(197);
+//const linkedListCollection = new LinkedList();
+// linkedListCollection.add(1);
+// linkedListCollection.add(100);
+// linkedListCollection.add(-1);
+// linkedListCollection.add(-115);
+// linkedListCollection.add(197);
 
-const sorter = new Sorter(linkedListCollection);
-sorter.sort();
-linkedListCollection.print();
+// const sorter = new Sorter(linkedListCollection);
+// sorter.sort();
+// linkedListCollection.print();
